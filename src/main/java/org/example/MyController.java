b6fc9313552d50f90650c0b1970110a30cd9e7ed
shashkist <shashkist@gmail.com>
2024-03-26 20:54:03 +0200

    Initial commit

---
+++ src/main/java/org/example/MyController.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+
+    public MyController() {
+        System.out.println("Controller Created!");
+    }
+}
